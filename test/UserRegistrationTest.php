be6fbd968cd45ae5044ae78d429133fb39198a49
Taiwola <seunolanitori@gmail.com>
2024-07-16 14:22:18 +0100

    chore:more assertions

--- test/UserRegistrationTest.php
+++ test/UserRegistrationTest.php
@@ -19,17 +19,37 @@ class UserRegistrationTest extends TestCase
     {
         $userRegistration = new UserRegistration('john_doe', 'john@example', 'Password123$');
         $this->assertFalse($userRegistration->validate());
+        $this->assertArrayHasKey('email', $userRegistration->getErrors());
+        $this->assertEquals('Invalid email format.', $userRegistration->getErrors()['email']);
     }
 
     public function testuser_when_password_is_format_is_incorrect()
     {
         $userRegistration = new UserRegistration('john_doe', 'john@example.com', 'Password');
         $this->assertFalse($userRegistration->validate());
+        $this->assertArrayHasKey('password', $userRegistration->getErrors());
+        $this->assertEquals('Password must contain at least one lowercase letter, one uppercase letter, and one digit, and be at least 8 characters long.', $userRegistration->getErrors()['password']);
     }
 
     public function test_return_error_when_field_is_missing()
     {
         $userRegistration = new UserRegistration('john_doe', 'john@example.com', '');
         $this->assertFalse($userRegistration->validate());
+    }
+
+    public function test_Missing_Username()
+    {
+        $userRegistration = new UserRegistration('', 'john@example.com', 'Password123$');
+        $this->assertFalse($userRegistration->validate());
+        $this->assertArrayHasKey('username', $userRegistration->getErrors());
+        $this->assertEquals('Username is required.', $userRegistration->getErrors()['username']);
+    }
+
+    public function test_Missing_Password()
+    {
+        $userRegistration = new UserRegistration('john_doe', 'john@example.com', '');
+        $this->assertFalse($userRegistration->validate());
+        $this->assertArrayHasKey('password', $userRegistration->getErrors());
+        $this->assertEquals('Password is required.', $userRegistration->getErrors()['password']);
     }
 }
